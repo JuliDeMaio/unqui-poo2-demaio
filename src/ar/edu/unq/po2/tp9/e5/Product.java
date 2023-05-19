90eefb00ed5851d25313d4bc6a695400ab5faa6b
Julian De Maio <julidema82@gmail.com>
2023-05-19 20:41:16 -0300

    fin tp-9

---
+++ src/ar/edu/unq/po2/tp9/e5/Product.java
@@ -0,0 +1,15 @@
+package ar.edu.unq.po2.tp9.e5;
+
+public class Product {
+
+	private float price;
+	private String name;
+
+	public float getPrice() {
+		return price;
+	}
+
+	public String getName() {
+		return name;
+	}	
+}
